cdb7896fb90a67e0be58b2e818a69b6f7745dd94
jisook <jisookkim1052@gmail.com>
2026-08-26 16:00:17 +0900

    Gemini 모모프 AI 분석 결과 스크린샷 추가델명 최신 버전으로 교체

---
+++ filter-env.sh
@@ -0,0 +1,2 @@
+if [ "$GIT_AUTHOR_NAME" = "wonseok0304" ]; then export GIT_AUTHOR_NAME="jisook"; export GIT_AUTHOR_EMAIL="jisookkim1052@gmail.com"; fi
+if [ "$GIT_COMMITTER_NAME" = "wonseok0304" ]; then export GIT_COMMITTER_NAME="jisook"; export GIT_COMMITTER_EMAIL="jisookkim1052@gmail.com"; fi
